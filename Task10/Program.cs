09f5d39e0c1dab5daac8c4a6cf61e097dd7e32b5
Ilya Nurullin <nurullinin@mail.ru>
2017-08-09 23:02:45 +0500

    The first version of solution

---
+++ Task10/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    int count = Input.readInt("Введите количество элементов в последовательности", x => x > 0,
+                        "Ожидалось натуральное число");
+
+                    bool isNormal = true;
+                    ReversedList list = new ReversedList(Input.readInt("Введите число"));
+                    for (int i = 0; i < count; i++)
+                    {
+                        int next = Input.readInt("Введите число");
+                        isNormal = next >= list.data;
+                        list = list.append(next);
+                    }
+
+                    if (isNormal)
+                    {
+                        Console.WriteLine("Последовательность упорядочена по неубыванию");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Новая коллекция: {0}", list);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ожидалось число.");
+                }
+            }
+        }
+    }
+}
---
+++ Task10/ReversedList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class ReversedList
+    {
+        public int data;
+        public ReversedList prev;
+
+        public ReversedList(int data, ReversedList prev = null)
+        {
+            this.data = data;
+            this.prev = prev;
+        }
+
+        public ReversedList append(int s)
+        {
+            return new ReversedList(s, this);
+        }
+
+        public override string ToString()
+        {
+            if (prev == null) return "null";
+            return data + "->" + prev.ToString();
+        }
+    }
+}
